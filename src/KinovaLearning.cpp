945c8f8eaac1df9152fc9012c6d2ea1e90cd915a
Khang Pham <phamthekhang632@gmail.com>
2026-02-09 17:53:29 +0900

    feat: joint_6oscillate to show controller's connection

--- src/KinovaLearning.cpp
+++ src/KinovaLearning.cpp
@@ -16,13 +16,37 @@ KinovaLearning::KinovaLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_
 
 bool KinovaLearning::run()
 {
-  postureTask->target(defaultPosture);
+  if(postureTask->eval().norm() < 0.02 && postureTask->speed().norm() < 0.01)
+  {
+    switchState();
+  }
   return mc_control::MCController::run();
 }
 
 void KinovaLearning::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
+}
+
+void KinovaLearning::switchState()
+{
+  switch (state_)
+  {
+  case IDLE:
+    postureTask->target(defaultPosture);
+    state_ = MOVE_UP;
+    break;
+
+  case MOVE_UP:
+    postureTask->target(upPosture);
+    state_ = MOVE_DOWN;
+    break;
+
+  case MOVE_DOWN:
+    postureTask->target(downPosture);
+    state_ = MOVE_UP;
+    break;
+  }
 }
 
 CONTROLLER_CONSTRUCTOR("KinovaLearning", KinovaLearning)
--- src/KinovaLearning.h
+++ src/KinovaLearning.h
@@ -2,8 +2,14 @@
 
 #include <mc_control/mc_controller.h>
 
-
 #include "api.h"
+
+enum ControlState 
+{
+  IDLE = 0,
+  MOVE_UP,
+  MOVE_DOWN
+};
 
 struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
 {
@@ -12,6 +18,8 @@ struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
   void reset(const mc_control::ControllerResetData & reset_data) override;
 
   private:
+    void switchState();
+    ControlState state_ = IDLE;
     std::map< std::string, std::vector< double >> defaultPosture = {
       {"joint_1", {0}},
       {"joint_2", {0}},
@@ -19,5 +27,14 @@ struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
       {"joint_4", {-M_PI * 3 / 4}},
       {"joint_5", {0}},
       {"joint_6", {M_PI / 4}}
+    };
+    double moveMag = M_PI / 6;
+    std::map< std::string, std::vector< double >> upPosture = 
+    {
+      {"joint_6", {M_PI / 4 + moveMag}}
+    };
+    std::map< std::string, std::vector< double >> downPosture = 
+    {
+      {"joint_6", {M_PI / 4 - moveMag}}
     };
 };
