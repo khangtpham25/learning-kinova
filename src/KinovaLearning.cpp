86ad0c52f6c4091f83b9630717e35770f3485e40
Khang Pham <phamthekhang632@gmail.com>
2026-02-09 09:24:18 +0100

    feat: add default posture

--- src/KinovaLearning.cpp
+++ src/KinovaLearning.cpp
@@ -4,15 +4,19 @@ KinovaLearning::KinovaLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_
 : mc_control::MCController(rm, dt)
 {
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
+  solver().addConstraintSet(selfCollisionConstraint);
+  solver().addConstraintSet(dynamicsConstraint);
+  solver().addConstraintSet(compoundJointConstraint);
+  postureTask->stiffness(0.5);
+  postureTask->weight(1);
   solver().addTask(postureTask);
-  solver().setContacts({{}});
 
   mc_rtc::log::success("KinovaLearning init done ");
 }
 
 bool KinovaLearning::run()
 {
+  postureTask->target(defaultPosture);
   return mc_control::MCController::run();
 }
 
--- src/KinovaLearning.h
+++ src/KinovaLearning.h
@@ -8,8 +8,16 @@
 struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
 {
   KinovaLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
-
   bool run() override;
-
   void reset(const mc_control::ControllerResetData & reset_data) override;
+
+  private:
+    std::map< std::string, std::vector< double >> defaultPosture = {
+      {"joint_1", {0}},
+      {"joint_2", {0}},
+      {"joint_3", {M_PI}},
+      {"joint_4", {-M_PI * 3 / 4}},
+      {"joint_5", {0}},
+      {"joint_6", {M_PI / 4}}
+    };
 };
