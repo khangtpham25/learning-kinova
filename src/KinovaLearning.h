c13fe06b54a2ea44f28c7b8b8c4ad6e67d8f7ac0
Khang Pham <phamthekhang632@gmail.com>
2026-03-12 17:44:44 +0900

    feat(yaml): update yaml to include all available robots

--- src/KinovaLearning.h
+++ src/KinovaLearning.h
@@ -4,7 +4,7 @@
 
 #include "api.h"
 
-enum ControlState 
+enum ControlState
 {
   IDLE = 0,
   MOVE_UP,
@@ -17,24 +17,14 @@ struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
   bool run() override;
   void reset(const mc_control::ControllerResetData & reset_data) override;
 
-  private:
-    void switchState();
-    ControlState state_ = IDLE;
-    std::map< std::string, std::vector< double >> defaultPosture = {
-      {"joint_1", {0}},
-      {"joint_2", {0}},
-      {"joint_3", {M_PI}},
-      {"joint_4", {-M_PI * 3 / 4}},
-      {"joint_5", {0}},
-      {"joint_6", {M_PI / 4}}
-    };
-    double moveMag = M_PI / 6;
-    std::map< std::string, std::vector< double >> upPosture = 
-    {
-      {"joint_6", {M_PI / 4 + moveMag}}
-    };
-    std::map< std::string, std::vector< double >> downPosture = 
-    {
-      {"joint_6", {M_PI / 4 - moveMag}}
-    };
+private:
+  void switchState();
+  ControlState state_ = IDLE;
+  std::map<std::string, std::vector<double>> defaultPosture = {{"joint_1", {1.5 * M_PI}}, {"joint_2", {0}},
+                                                               {"joint_3", {M_PI}},       {"joint_4", {-0.75 * M_PI}},
+                                                               {"joint_5", {0}},          {"joint_6", {0.25 * M_PI}},
+                                                              {"joint_7", {M_PI}}};
+  double moveMag = M_PI / 6;
+  std::map<std::string, std::vector<double>> upPosture = {{"joint_6", {M_PI / 4 + moveMag}}};
+  std::map<std::string, std::vector<double>> downPosture = {{"joint_6", {M_PI / 4 - moveMag}}};
 };
