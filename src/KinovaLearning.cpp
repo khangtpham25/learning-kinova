c521239646322c1073124250ec57460f385aec9c
Khang Pham <phamthekhang632@gmail.com>
2026-05-28 12:32:03 +0200

    feat: minimal demonstration for floating base

--- src/KinovaLearning.cpp
+++ src/KinovaLearning.cpp
@@ -1,84 +1,47 @@
 #include "KinovaLearning.h"
 
+#include <mc_rbdyn/RobotLoader.h>
+
 KinovaLearning::KinovaLearning(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config)
-    : mc_control::MCController(std::move(rm), dt)
+    : mc_control::MCController({std::move(rm), mc_rbdyn::RobotLoader::get_robot_module("dingo"),
+                                mc_rbdyn::RobotLoader::get_robot_module("env/ground")},
+                               dt)
 {
   solver().addConstraintSet(contactConstraint);
   solver().addConstraintSet(selfCollisionConstraint);
-  solver().addConstraintSet(dynamicsConstraint);
-  solver().addConstraintSet(compoundJointConstraint);
   postureTask->stiffness(0.5);
   postureTask->weight(1);
   solver().addTask(postureTask);
+  solver().setContacts({{}});
+
+  Eigen::Vector6d dof = Eigen::Vector6d::Ones();
+  double friction = mc_rbdyn::Contact::defaultFriction;
+  dof[2] = 0.0;
+  dof[3] = 0.0;
+  dof[4] = 0.0;
+  addContact({"dingo", "ground", "Base", "AllGround", friction, dof});
+  addContact({"dingo", "kinova", "Base", "Base"});
 
   mc_rtc::log::success("KinovaLearning init done ");
 }
 
 bool KinovaLearning::run()
 {
-  bool storing = true;
-  if (storing)
-  {
-    if (postureTask->eval().norm() < 0.05 && postureTask->speed().norm() < 0.05)
-    {
-      storeState();
-    }
-  }
-  else
-  {
-    if (postureTask->eval().norm() < 0.02 && postureTask->speed().norm() < 0.01)
-    {
-      switchState();
-    }
-  }
   return mc_control::MCController::run();
 }
 
 void KinovaLearning::reset(const mc_control::ControllerResetData &reset_data)
 {
   mc_control::MCController::reset(reset_data);
-}
 
-void KinovaLearning::storeState()
-{
-  switch (store_state_)
-  {
-  case START:
-    postureTask->target(store_pose_start_);
-    store_state_ = FOLD;
-    break;
+  robots().robot("kinova").posW(sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(0.0, 0.0, 0.5)));
+  robots().robot("dingo").posW(sva::PTransformd(sva::RotZ(0.0), Eigen::Vector3d(-0.25, 0.0, 0.0)));
 
-  case FOLD:
-    postureTask->target(store_pose_fold_);
-    store_state_ = BEND;
-    break;
+  dingo_base_task_ = std::make_shared<mc_tasks::TransformTask>("base_link", robots(), 1, 2.0, 1000);
 
-  case BEND:
-    postureTask->target(store_pose_bend_);
-    // state_ = MOVE_UP;
-    break;
-  }
-}
-
-void KinovaLearning::switchState()
-{
-  switch (state_)
-  {
-  case IDLE:
-    postureTask->target(default_posture_);
-    state_ = MOVE_UP;
-    break;
-
-  case MOVE_UP:
-    postureTask->target(up_posture_);
-    state_ = MOVE_DOWN;
-    break;
-
-  case MOVE_DOWN:
-    postureTask->target(down_posture_);
-    state_ = MOVE_UP;
-    break;
-  }
+  solver().addTask(dingo_base_task_);
+  postureTask->target({{"shoulder_lift_joint", {-M_PI / 2}}});
+  dingo_base_task_->target({Eigen::Vector3d(1.5, 0.0, 0.0)});
 }
 
 CONTROLLER_CONSTRUCTOR("KinovaLearning", KinovaLearning)
--- src/KinovaLearning.h
+++ src/KinovaLearning.h
@@ -1,22 +1,9 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
+#include <mc_tasks/TransformTask.h>
 
 #include "api.h"
-
-enum ControlState
-{
-  IDLE = 0,
-  MOVE_UP,
-  MOVE_DOWN
-};
-
-enum StoreState
-{
-  START = 0,
-  FOLD,
-  BEND
-};
 
 struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
 {
@@ -25,22 +12,5 @@ struct KinovaLearning_DLLAPI KinovaLearning : public mc_control::MCController
   void reset(const mc_control::ControllerResetData &reset_data) override;
 
 private:
-  // ControlState
-  void switchState();
-  ControlState state_ = IDLE;
-  std::map<std::string, std::vector<double>> default_posture_ = {
-      {"joint_1", {1.5 * M_PI}}, {"joint_2", {0}},           {"joint_3", {M_PI}}, {"joint_4", {-0.75 * M_PI}},
-      {"joint_5", {0}},          {"joint_6", {0.25 * M_PI}}, {"joint_7", {M_PI}}};
-  double move_mag_ = M_PI / 6;
-  std::map<std::string, std::vector<double>> up_posture_ = {{"joint_6", {M_PI / 4 + move_mag_}}};
-  std::map<std::string, std::vector<double>> down_posture_ = {{"joint_6", {M_PI / 4 - move_mag_}}};
-
-  // StoreState
-  void storeState();
-  StoreState store_state_ = START;
-  std::map<std::string, std::vector<double>> store_pose_start_ = {
-      {"joint_1", {0.5 * M_PI}}, {"joint_2", {0}},   {"joint_3", {0}}, {"joint_4", {0}},
-      {"joint_5", {0}},          {"joint_6", {2.0}}, {"joint_7", {0}}};
-  std::map<std::string, std::vector<double>> store_pose_fold_ = {{"joint_4", {2.3}}, {"joint_5", {0.4}}};
-  std::map<std::string, std::vector<double>> store_pose_bend_ = {{"joint_1", {0.0}}, {"joint_2", {0.5 * M_PI}}};
+  std::shared_ptr<mc_tasks::TransformTask> dingo_base_task_;
 };
